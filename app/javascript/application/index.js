6b78306543cab171373af9fa196deabf231a2403
Joel Moss <joel@developwithstyle.com>
2017-05-08 17:07:35 +0100

    Changed "Orgs" header menu item to a dropdown of orgs

--- app/javascript/application/index.js
+++ app/javascript/application/index.js
@@ -1,8 +1,13 @@
 import $ from 'jquery'
 $.fn.transition = require('semantic-ui-transition')
+$.fn.dropdown = require('semantic-ui-dropdown')
 
 $(function () {
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade')
   })
+
+  // $('.ui.dropdown').dropdown({
+  //   on: 'hover'
+  // })
 })
